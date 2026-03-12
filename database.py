2846f4b5d6bfe0cef1493210c06b03ed18bff552
Meriyei Manosalva <meriyeilanena@gmail.com>
2026-03-12 17:41:10 -0500

    fix variables mysql railway

--- database.py
+++ database.py
@@ -1,12 +1,21 @@
 import mysql.connector
+from mysql.connector import Error
 from config import Config
 
 def get_connection():
-    conexion = mysql.connector.connect(
-        host=Config.MYSQL_HOST,
-        user=Config.MYSQL_USER,
-        password=Config.MYSQL_PASSWORD,
-        database=Config.MYSQL_DB,
-        port=Config.MYSQL_PORT
-    )
-    return conexion
+    try:
+        conexion = mysql.connector.connect(
+            host=Config.MYSQL_HOST,
+            user=Config.MYSQL_USER,
+            password=Config.MYSQL_PASSWORD,
+            database=Config.MYSQL_DB,
+            port=Config.MYSQL_PORT
+        )
+        if conexion.is_connected():
+            print("Conexión a MySQL exitosa")
+            return conexion
+    except Error as e:
+        # Aquí imprimimos el error para que Railway lo muestre en el Deploy Log
+        print(f"Error al conectar con MySQL: {e}")
+        # Lanza una excepción para que Flask sepa que falló la conexión
+        raise ConnectionError(f"No se pudo conectar a la base de datos: {e}")
