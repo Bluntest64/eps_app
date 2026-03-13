5f9a7ff53c2b64f30aeafa4a94c654dae403bf62
Bluntest64 <123kevindavidgomezposada@gmail.com>
2026-03-12 23:19:52 -0500

    fix mysql database env

--- config.py
+++ config.py
@@ -5,5 +5,5 @@ class Config:
     MYSQL_HOST = os.getenv("MYSQLHOST")
     MYSQL_USER = os.getenv("MYSQLUSER")
     MYSQL_PASSWORD = os.getenv("MYSQLPASSWORD")
-    MYSQL_DB = os.getenv("MYSQL_DATABASE")
+    MYSQL_DB = os.getenv("MYSQLDATABASE")
     MYSQL_PORT = int(os.getenv("MYSQLPORT", 3306))
